03bdc3d7295a46559cef4691f482b770dd259e83
KevinGiesberts <kevin.giesberts@gmail.com>
2022-03-22 20:51:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,59 +1,50 @@
 import random
 
+# What does the algorithm need to solve?
+#   The word length
+#   The syllable_order
+#   What words mean what
+#   The grammar of the language
+
 
 possible_consonants = ['m', 'n', 'p', 't', 'k', 's', 'w', 'l', 'j']
 possible_vowels = ['i', 'u', 'e', 'o', 'a']
 
-def generate_words(possible_consonants, possible_vowels):
-    words = []
+
+def check():
+    return int(input("How readable is this word? (The higher the score the better) You can use http://ipa-reader.xyz/ as an helper. "))
+
+def generate_word(syllable_order):
+    for g in range(5):
+        if syllable_order[g] == "V"
+
+def fitness(syllable_order):
+    generate_word(syllable_order)
+    ans = check()
+
+    if ans == 100:
+        return 0
+    else:
+        return abs(1/ans)
+
+
+#syllable_order = ''
+word_length = 5
+solutions = []
+for i in range(1000):
     syllable_order = ''
-    word_length = random.randint(3, 6)
     for i in range(word_length):
-        if random.randint(0, 10) < 5:
+        if random.random() < .5:
             syllable_order += "V"
         else:
-            syllable_order += "C"
+            syllable_order += "V"
 
-    for i in range(1):#random.randint(10, 50)):
-        word = ''
-        for y in range(word_length):
-            #print(syllable_order[y])
-            if syllable_order[y] == "C":
-                word += random.choice(possible_consonants)
-            else:
-                word += random.choice(possible_vowels)
+    solutions.append(syllable_order)
 
-        words.append(word)
+max_generations = 1000
 
-    return words, syllable_order
+for i in range(max_generations):
+    rankedSolutions = []
 
-def algorithm(possible_consonants, possible_vowels):
-    gen = 0
-    while True:
-        words, syllable_order = generate_words(possible_consonants, possible_vowels)
-        score = 0
-
-        for i in words:
-            print(i)
-            while True:
-                try:
-                    inp = int(input("How readable is this word (scale -100 to 100)? "))
-                    break
-                except:
-                    print("Input must be an integer")
-                    continue
-
-            score += inp
-
-        if score >= 5000:
-            break
-        elif gen >= 1000:
-            break
-
-        gen += 1
-
-    print(words)
-    print(syllable_order)
-
-
-algorithm(possible_consonants, possible_vowels)
+    for y in solutions:
+        rankedSolutions.append(fitness())
