895a226005c77727928a53662f0c1bbed4416c0e
KevinGiesberts <kevin.giesberts@gmail.com>
2022-03-22 19:16:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ possible_vowels = ['i', 'u', 'e', 'o', 'a']
 def generate_words(possible_consonants, possible_vowels):
     words = []
     syllable_order = ''
-    word_length = random.randint(3, 10)
+    word_length = random.randint(3, 6)
     for i in range(word_length):
         if random.randint(0, 10) < 5:
             syllable_order += "V"
         else:
             syllable_order += "C"
 
-    for i in range(random.randint(100, 1000)):
+    for i in range(1):#random.randint(10, 50)):
         word = ''
         for y in range(word_length):
             #print(syllable_order[y])
@@ -32,6 +32,18 @@ def algorithm(possible_consonants, possible_vowels):
     while True:
         words, syllable_order = generate_words(possible_consonants, possible_vowels)
         score = 0
+
+        for i in words:
+            print(i)
+            while True:
+                try:
+                    inp = int(input("How readable is this word (scale -100 to 100)? "))
+                    break
+                except:
+                    print("Input must be an integer")
+                    continue
+
+            score += inp
 
         if score >= 5000:
             break
