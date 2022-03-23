447c89a5ab599955b5f41952943f39682afbc815
KevinGiesberts <kevin.giesberts@gmail.com>
2022-03-23 13:04:03 +0100

    Update word_meaning.py

--- word_meaning.py
+++ word_meaning.py
@@ -7,12 +7,14 @@ class Dictionary:
         self.access_api = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}")
         self.get_word = BeautifulSoup(self.access_api.content, 'html.parser')
         print(self.get_word)
+        self.get_meaning()
 
     def get_synonyms(self):
         pass
 
     def get_meaning(self):
-        pass
+        self.meaning = self.get_word.find_all()
+        print(self.meaning)
 
 
 Dictionary('this')
