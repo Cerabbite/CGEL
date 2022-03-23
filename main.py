e99f00ffc2cec844837cf2ff9d4cf94db35676be
KevinGiesberts <kevin.giesberts@gmail.com>
2022-03-23 10:48:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import random
 #   What words mean what (Based upon the top 10.000 most used English words)
 #   Pronouns
 
-# The algorithm will generate between 1 and 500 different words and 15.000 sample sentences based upon the 15.000 most used sentences in English.
+# The algorithm will generate between 1 and 500 different words and 1.000 sample sentences based upon the 1.000 most used sentences in English.
 
 possible_consonants = ['m', 'n', 'p', 't', 'k', 's', 'w', 'l', 'j']
 possible_vowels = ['i', 'u', 'e', 'o', 'a']
