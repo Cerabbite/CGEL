219641573f941b8f41f49dd3c9b0adb6c60f9216
KevinGiesberts <kevin.giesberts@gmail.com>
2022-03-23 11:13:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,17 @@ import random
 
 # The algorithm will generate between 1 and 500 different words and 1.000 sample sentences based upon the 1.000 most used sentences in English.
 
+
+
+"""
+Step 1: Create a gene that contains all the information for the syllalble order, exact word length, pronouns structure, grammar rules
+Step 2: Generate the dictionary from the gene with 1 to 500 words
+Step 3: Generate grammatically correct sentences from the 1 to 500 words and the sample sentences in English
+Step 4: Give the text to a human and make it give feedback. The feedback will be a score between 0 and 100 for the syllable order, grammar rules, pronouns, readability etc
+Step 5: Create a new language with the input of the human
+Repeat step 1 to 5 upuntill the language is super easy to learn
+"""
+
 possible_consonants = ['m', 'n', 'p', 't', 'k', 's', 'w', 'l', 'j']
 possible_vowels = ['i', 'u', 'e', 'o', 'a']
 
