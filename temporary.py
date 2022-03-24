f1a02c50428a943888fc282f6da62829f35d5bc1
KevinGiesberts <kevin.giesberts@gmail.com>
2022-03-24 17:50:37 +0100

    Update temporary.py

--- temporary.py
+++ temporary.py
@@ -2,17 +2,30 @@ import nltk
 from nltk.corpus import wordnet
 from difflib import SequenceMatcher
 
-a = wordnet.synsets('management')
-a = a[0].definition()
-b = wordnet.synsets('administration')
-b = b[0].definition()
+word_a = "management"
+word_b = "administration"
 
-a = "Hello, how are you?"
-b = "How are you?"
+a = wordnet.synsets(word_a)
+b = wordnet.synsets(word_b)
 
 # maybe use this instead of difflib https://stackoverflow.com/questions/65199011/is-there-a-way-to-check-similarity-between-two-full-sentences-in-python
-ratio = SequenceMatcher(None, a, b).ratio()
+#ratio = SequenceMatcher(None, a[0].definition(), b[0].definition()).ratio()
 
-print(a)
-print(b)
-print(ratio)
+#print(a)
+#print(b)
+#print(ratio)
+#imp https://www.youtube.com/watch?v=zu9AcxfklrI
+
+synonyms = []
+for syn in wordnet.synsets(word_a):
+    for lemma in syn.lemmas():
+        synonyms.append(lemma.name())
+
+print(synonyms)
+
+synonyms = []
+for syn in wordnet.synsets(word_b):
+    for lemma in syn.lemmas():
+        synonyms.append(lemma.name())
+
+print(synonyms)
