40246d9e08b00ab331cbb58faf84c226d91c8922
Kevin <98265679+Cerabbite@users.noreply.github.com>
2022-03-24 08:57:28 +0100

    Update word_meaning.py

--- word_meaning.py
+++ word_meaning.py
@@ -1,21 +1,22 @@
 #https://api.dictionaryapi.dev/api/v2/entries/en/<word>
 from bs4 import BeautifulSoup
 import requests
+import json
 
 class Dictionary:
     def __init__(self, word):
         self.access_api = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}")
-        self.get_word = self.access_api.text
-        self.word = self.get_word #self.get_word.decode("utf-8")
-        print(self.word)
+        self.get_word = self.access_api.json()
+        self.word = self.get_word[0] #self.get_word.decode("utf-8")
+        #print(self.word)
         self.get_meaning()
 
     def get_synonyms(self):
         pass
 
     def get_meaning(self):
-        #self.meaning = self.word[]
-        print(self.word.find("meanings"))
+        self.meaning = self.word["meanings"][0]
+        print(self.meaning["definitions"])
         #print(self.meaning)
 
 
