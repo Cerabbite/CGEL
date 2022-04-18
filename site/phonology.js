8fa0fe7c3e55ea668ca98f5fcdc79bd62c26ff3b
KevinGiesberts <kevin.giesberts@gmail.com>
2022-04-18 11:33:37 +0200

    Update phonology.js

--- site/phonology.js
+++ site/phonology.js
@@ -1,29 +1,15 @@
 const getLanguages = async () => {
+  const parent = document.querySelector('#languages');
 
+  const languages = await fetch('languages.txt').then(x => x.text()).then((data) => data.split(/\r?\n/).sort());
 
-const parent = document.querySelector('#languages');
-
-
-const languages = await fetch('languages.txt').then(x => x.text()).then((data) => data.split(/\r?\n/).sort());
-
-If (languages) {
-
-
-    languages.forEach((language) => {
-
-
- const option = document.createElement('option');
-
-
-option.value = language;
-
-
- option.text = language;
-
-
-       parent.appendChild(option);
-
-    })
-}
+  If (languages) {
+      languages.forEach((language) => {
+        const option = document.createElement('option');
+        option.value = language;
+        option.text = language;
+        parent.appendChild(option);
+      })
+  }
 
 }
