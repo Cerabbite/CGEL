9caa64c7c58745b39bffd71992254aedada65b31
KevinGiesberts <kevin.giesberts@gmail.com>
2022-04-26 14:48:44 +0200

    Update word_generator.py

--- site/word_generator.py
+++ site/word_generator.py
@@ -1,6 +1,23 @@
 import random
 
 C = "M N P T K S W L J"
+C = C.split(" ")
 V = "A E I O U"
+V = V.split(" ")
+print(C)
+print(V)
 
-Syllable_structure = ["CV", "VC", "CV", "CCV", "VCC", "CCVC", "CVCC", "CCVCC"]
+syllable_structure = ["CV", "VC", "CV", "CCV", "VCC", "CCVC", "CVCC", "CCVCC"]
+
+amm_words = 100
+
+for g in range(amm_words):
+    for i in syllable_structure:
+        word = ""
+        for y in i:
+            if y == "C":
+                word += random.choice(C)
+            elif y == "V":
+                word += random.choice(V)
+
+        print(word)
